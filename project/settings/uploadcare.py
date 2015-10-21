0454204f75059a5d40e3ef37926bfcd1d79e7f37
zero13cool <zero13cool@yandex.ru>
2015-10-21 17:44:19 +0300

    Updated widget version

--- project/settings/uploadcare.py
+++ project/settings/uploadcare.py
@@ -5,6 +5,7 @@ import os
 UPLOADCARE = {
     'pub_key': os.environ.get('UPLOADCARE_PUBLIC_KEY', 'demopublickey'),
     'secret': os.environ.get('UPLOADCARE_SECRET_KEY', 'demoprivatekey'),
+    'widget_version': '2.5.5',
 }
 
 FORBIDDEN_EXTENSIONS = ('.exe',)
